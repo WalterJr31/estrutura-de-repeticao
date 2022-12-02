6445c7f5426942f8bfbb1b564a3b145d3ac46a7a
Walter Junior <walter.asjunior31@gmail.com>
2022-12-01 22:13:58 -0300

    estrutura de repetição

---
+++ 02/app.js
@@ -0,0 +1,8 @@
+function Numeros(){
+    //enquanto
+    let contador = 51
+    while(contador <= 99){
+        document.getElementById("resposta").innerHTML += "Número" + contador + "<br>"
+        contador = contador + 1
+    }   
+}
---
+++ 05/app.js
@@ -0,0 +1,8 @@
+function Hexa(){
+    //enquanto
+    let contador = 0
+    while(contador <= 14){
+        document.getElementById("resposta").innerHTML += "O hexa vem, algum dia, mas vem" + "<br>"
+        contador = contador +1
+    }   
+}
